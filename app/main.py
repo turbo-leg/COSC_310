6455febc5263c66175bd1741423c9c2a2f2d3b5d
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-10 00:46:10 -0700

    Altered main to pass pylint tests

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.controllers import user_controller
 from app.controllers import order_controller
-from app.controllers import user_controller, menu_controller
+from app.controllers import menu_controller
 from app import database
 
 @asynccontextmanager
