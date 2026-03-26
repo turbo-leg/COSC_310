049f2b962e5710814bcd5e44743889318f75cd05
sk114343 <159204395+sk114343@users.noreply.github.com>
2026-03-25 23:17:00 -0700

    Pylint

--- app/auth.py
+++ app/auth.py
@@ -5,6 +5,8 @@ Authentication utilities.
 from fastapi import Header, HTTPException
 from app.token import decode_token
 
+BEARER_PREFIX = "Bearer "
+
 def get_user(authorization: str = Header(None)):
     """
     Extracts user information from the Authorization header and verifies the token.
@@ -12,7 +14,6 @@ def get_user(authorization: str = Header(None)):
     if not authorization:
         raise HTTPException(status_code=401)
 
-    BEARER_PREFIX = "Bearer "
     token = authorization.replace(BEARER_PREFIX, "")
     user = decode_token(token)
 
